8c55c42c0d1a380d78f7346b16d800a40abc24e2
Abel <fazea4930@gmail.com>
2025-11-27 08:28:47 -0800

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "media", // auto dark mode based on system settings
+  plugins: [],
+};
